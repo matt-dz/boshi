a066f81b6f39fbfc0437291d99d221e9329025dc
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2025-03-02 01:25:29 -0500

    update staging to have the same fake data as the local service

--- frontend/lib/data/services/api/api_client.dart
+++ frontend/lib/data/services/api/api_client.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:frontend/utils/result.dart';
 import 'package:frontend/domain/models/post/post.dart';
+import 'package:frontend/domain/models/reply/reply.dart';
 import 'package:frontend/domain/models/user/user.dart';
 
 import 'package:frontend/utils/logger.dart';
@@ -28,7 +29,7 @@ class ApiClient {
         ),
         content: 'Hello, world!',
         timestamp: DateTime.now(),
-        reactions: {},
+        reactions: {'🔥', '❤️', '👍'},
         comments: [],
         id: '1',
         title: 'Post 1',
@@ -40,13 +41,79 @@ class ApiClient {
           school: 'Stanford University',
         ),
         content: '''
-              Lorem ipsum dolor sit amet, consectetur adipiscing
-              elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua.
-              ''',
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.''',
         timestamp: DateTime.now(),
         reactions: {},
-        comments: [],
+        comments: [
+          Reply(
+            id: '100',
+            author: User(
+              id: '1',
+              username: 'anonymous1',
+              school: 'University of Florida',
+            ),
+            content: 'This is a reply',
+            timestamp: DateTime.now(),
+            reactions: {},
+            replyToId: '1',
+            comments: [
+              Reply(
+                id: '101',
+                author: User(
+                  id: '2',
+                  username: 'anonymous2',
+                  school: 'Stanford University',
+                ),
+                content: 'This is a reply to a reply',
+                timestamp: DateTime.now(),
+                reactions: {},
+                replyToId: '100',
+                comments: [
+                  Reply(
+                    id: '102',
+                    author: User(
+                      id: '3',
+                      username: 'anonymous3',
+                      school: 'University of Washington',
+                    ),
+                    content: 'This is a reply to a reply to a reply',
+                    timestamp: DateTime.now(),
+                    reactions: {},
+                    replyToId: '101',
+                    comments: [],
+                  ),
+                ],
+              ),
+              Reply(
+                id: '103',
+                author: User(
+                  id: '4',
+                  username: 'anonymous4',
+                  school: 'Princeton University',
+                ),
+                content: 'This is a reply to a reply',
+                timestamp: DateTime.now(),
+                reactions: {},
+                replyToId: '100',
+                comments: [
+                  Reply(
+                    id: '104',
+                    author: User(
+                      id: '3',
+                      username: 'anonymous3',
+                      school: 'University of Washington',
+                    ),
+                    content: 'This is a reply to a reply to a reply',
+                    timestamp: DateTime.now(),
+                    reactions: {},
+                    replyToId: '103',
+                    comments: [],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
         title: 'Post 2',
         id: '2',
       ),
@@ -57,13 +124,7 @@ class ApiClient {
           school: 'University of Washington',
         ),
         content: '''
-              Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation
-              ullamco laboris nisi ut aliquip ex ea commodo
-              consequat.
-              ''',
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.''',
         timestamp: DateTime.now(),
         reactions: {},
         comments: [],
@@ -77,8 +138,7 @@ class ApiClient {
           school: 'Princeton University',
         ),
         content: '''
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              ''',
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit , sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.''',
         timestamp: DateTime.now(),
         reactions: {},
         comments: [],
@@ -92,10 +152,7 @@ class ApiClient {
           school: 'Harvard University',
         ),
         content: '''
-              Lorem ipsum dolor sit amet, consectetur adipiscing
-              elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua.
-              ''',
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.''',
         timestamp: DateTime.now(),
         reactions: {},
         comments: [],
@@ -109,10 +166,7 @@ class ApiClient {
           school: 'University of California, Berkely',
         ),
         content: '''
-              Lorem ipsum dolor sit amet, consectetur adipiscing
-              elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua.
-              ''',
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.''',
         timestamp: DateTime.now(),
         reactions: {},
         comments: [],
@@ -126,10 +180,7 @@ class ApiClient {
           school: 'University of Southern California',
         ),
         content: '''
-              Lorem ipsum dolor sit amet, consectetur adipiscing
-              elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua.
-              ''',
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.''',
         timestamp: DateTime.now(),
         reactions: {},
         comments: [],
@@ -143,10 +194,7 @@ class ApiClient {
           school: 'University of California, Los Angeles',
         ),
         content: '''
-              Lorem ipsum dolor sit amet, consectetur adipiscing
-              elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua.
-              ''',
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.''',
         timestamp: DateTime.now(),
         reactions: {},
         comments: [],
@@ -160,10 +208,7 @@ class ApiClient {
           school: 'University of Pennsylvania',
         ),
         content: '''
-              Lorem ipsum dolor sit amet, consectetur adipiscing
-              elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua.
-              ''',
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.''',
         timestamp: DateTime.now(),
         reactions: {},
         comments: [],
@@ -177,10 +222,7 @@ class ApiClient {
           school: 'Massachusetts Institute of Technology',
         ),
         content: '''
-              Lorem ipsum dolor sit amet, consectetur adipiscing
-              elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua.
-              ''',
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.''',
         timestamp: DateTime.now(),
         reactions: {},
         comments: [],
