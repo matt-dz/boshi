30690f4be19774ae4992a21a62826157119b3663
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2025-03-05 23:09:41 -0500

    refactor reactions, add horizontal dots, and plus button

--- frontend/lib/ui/home/widgets/content_item.dart
+++ frontend/lib/ui/home/widgets/content_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
+
 import 'package:frontend/domain/models/post/post.dart';
 import 'package:frontend/domain/models/content_item/content_item.dart';
 import 'package:frontend/domain/models/reaction/reaction.dart';
@@ -6,13 +8,18 @@ import 'package:frontend/utils/content_item.dart';
 
 import 'package:frontend/utils/logger.dart';
 
-class ReactionEmote extends StatelessWidget {
-  const ReactionEmote({
+// TODO: Figure out padding issue
+class ReactionWrapper extends StatelessWidget {
+  const ReactionWrapper({
     super.key,
-    required this.reaction,
+    required this.child,
+    this.onPressed,
+    this.padding = const EdgeInsets.fromLTRB(8, 12, 8, 12),
   });
 
-  final Reaction reaction;
+  final Widget child;
+  final void Function()? onPressed;
+  final EdgeInsetsGeometry padding;
 
   @override
   Widget build(BuildContext context) {
@@ -26,69 +33,61 @@ class ReactionEmote extends StatelessWidget {
         boxShadow: [
           BoxShadow(
             color: Colors.grey.shade400,
-            blurRadius: 4,
-            offset: Offset(0, 0),
+            blurRadius: 3,
+            offset: Offset(0, 2),
           ),
         ],
       ),
       child: OutlinedButton(
         style: OutlinedButton.styleFrom(
-          padding: EdgeInsets.fromLTRB(8, 12, 8, 12),
-          minimumSize: Size(0, 0),
+          padding: padding,
+          fixedSize: Size(double.infinity, 24),
           side: BorderSide(
             color: Colors.grey,
           ),
         ),
-        onPressed: () {
-          logger.d('${reaction.emote} pressed!');
-        },
-        child: Text(
-          '${reaction.emote} ${reaction.count}',
-        ),
+        onPressed: onPressed,
+        child: child,
       ),
     );
   }
 }
 
-class ContentItemWidget extends StatelessWidget {
-  const ContentItemWidget({
+class ReactionEmote extends StatelessWidget {
+  const ReactionEmote({
     super.key,
-    required this.post,
-    this.replyIndent = 0,
+    required this.reaction,
   });
 
-  final ContentItem post;
-  final int replyIndent;
+  final Reaction reaction;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(8 + replyIndent * 50, 16, 8, 16),
-      child: Container(
-        padding: EdgeInsets.all(8),
-        width: 450,
-        constraints: BoxConstraints(
-          minWidth: 350,
-        ),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(
-            Radius.circular(10),
-          ),
-          border: Border.all(
-            color: Colors.grey,
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.shade400,
-              blurRadius: 4,
-              offset: Offset(0, 2),
-            ),
-          ],
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          spacing: 4,
+    return ReactionWrapper(
+      onPressed: () {
+        logger.d('${reaction.emote} pressed!');
+      },
+      child: Text(
+        '${reaction.emote} ${reaction.count}',
+      ),
+    );
+  }
+}
+
+class ContentItemHeader extends StatelessWidget {
+  const ContentItemHeader({
+    super.key,
+    required this.post,
+  });
+
+  final ContentItem post;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
           children: [
             Text.rich(
               TextSpan(
@@ -119,27 +118,124 @@ class ContentItemWidget extends StatelessWidget {
                 ],
               ),
             ),
-            if (post is Post)
-              Text(
-                (post as Post).title,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
+            Spacer(),
+            IconButton(
+              icon: Icon(
+                PhosphorIconsRegular.dotsThreeVertical,
+                size: 20,
               ),
+              onPressed: () {
+                logger.d('dots pressed!');
+              },
+            ),
+          ],
+        ),
+        if (post is Post)
+          Text(
+            (post as Post).title,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+      ],
+    );
+  }
+}
+
+class AddReactionButton extends StatelessWidget {
+  const AddReactionButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ReactionWrapper(
+      onPressed: () {
+        logger.d('plus pressed!');
+      },
+      child: Icon(
+        PhosphorIconsRegular.plus,
+        size: 20,
+        color: Colors.black,
+      ),
+    );
+  }
+}
+
+class ContentItemFooter extends StatelessWidget {
+  const ContentItemFooter({
+    super.key,
+    required this.post,
+  });
+
+  final ContentItem post;
+
+  @override
+  Widget build(BuildContext build) {
+    final reactions = post.reactions.map(
+      (reaction) => ReactionEmote(reaction: reaction),
+    );
+
+    return Padding(
+      padding: EdgeInsets.only(top: 8),
+      child: Wrap(
+        spacing: 6,
+        runSpacing: 6,
+        children: [
+          ...reactions,
+          AddReactionButton(),
+        ],
+      ),
+    );
+  }
+}
+
+class ContentItemWidget extends StatelessWidget {
+  const ContentItemWidget({
+    super.key,
+    required this.post,
+    this.replyIndent = 0,
+    this.shadowColor = Colors.grey,
+  });
+
+  final ContentItem post;
+  final int replyIndent;
+  final Color shadowColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(8 + replyIndent * 50, 16, 8, 16),
+      child: Container(
+        padding: EdgeInsets.all(8),
+        width: 450,
+        constraints: BoxConstraints(
+          minWidth: 350,
+        ),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(
+            Radius.circular(10),
+          ),
+          border: Border.all(
+            color: Colors.grey,
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: shadowColor,
+              blurRadius: 4,
+              offset: Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 4,
+          children: [
+            ContentItemHeader(post: post),
             Text(
               post.content,
             ),
-            Padding(
-              padding: EdgeInsets.only(top: 8),
-              child: Row(
-                spacing: 6,
-                children: [
-                  for (final reaction in post.reactions)
-                    ReactionEmote(reaction: reaction),
-                ],
-              ),
-            ),
+            ContentItemFooter(post: post),
           ],
         ),
       ),
