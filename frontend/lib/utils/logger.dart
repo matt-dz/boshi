e7118473c13c6dd3fd836868891dfa11d7209506
Alex Eum <aeum1003@gmail.com>
2025-04-15 20:10:55 -0400

    feat: add filter to log everything

--- frontend/lib/utils/logger.dart
+++ frontend/lib/utils/logger.dart
@@ -1,8 +1,18 @@
 import 'package:logger/logger.dart';
 
 class AppLogger extends Logger {
-  static final Logger _logger = Logger();
+  static final Logger _logger = Logger(
+    filter: _AlwaysLogFilter(),
+    printer: PrettyPrinter(),
+  );
+
   static Logger get instance => _logger;
 }
 
 final logger = AppLogger.instance;
+
+/// Always logs everything, even in release mode.
+class _AlwaysLogFilter extends LogFilter {
+  @override
+  bool shouldLog(LogEvent event) => true;
+}
