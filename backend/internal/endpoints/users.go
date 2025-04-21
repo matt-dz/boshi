402793c4bf48668761e01eb64b613389c3933b99
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2025-04-21 18:07:17 -0400

    refactor: move GetUserById to users.go

--- backend/internal/endpoints/users.go
+++ backend/internal/endpoints/users.go
@@ -102,32 +102,3 @@ func GetUsersByID(w http.ResponseWriter, r *http.Request) {
 		Users: convertedResponse,
 	})
 }
-
-func GetUserByID(w http.ResponseWriter, r *http.Request) {
-	ctx := context.Background()
-	db := database.GetDb(ctx)
-	sqlcDb := sqlc.New(db)
-
-	log.DebugContext(r.Context(), "Getting user")
-
-	userID := r.PathValue("user_id")
-	if userID == "" {
-		log.ErrorContext(r.Context(), "No user id specified")
-		http.Error(w, "No user id specified", http.StatusBadRequest)
-		return
-	}
-
-	userResponse, err := sqlcDb.GetUser(ctx, userID)
-	if errors.Is(err, pgx.ErrNoRows) {
-		log.ErrorContext(r.Context(), "No row returned - user does not exist")
-		http.Error(w, "User does not exist", http.StatusNotFound)
-		return
-	} else if err != nil {
-		log.ErrorContext(r.Context(), "Failed to get user", slog.Any("error", err))
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	// Map the result to the User struct
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(getUserResponse(userResponse))
-}
