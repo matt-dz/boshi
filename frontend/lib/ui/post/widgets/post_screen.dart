fa4191ba10712540ba3fa6fc94259930edb87bce
Alex Eum <aeum1003@gmail.com>
2025-04-20 22:14:19 -0400

    fix: add notifiable

--- frontend/lib/ui/post/widgets/post_screen.dart
+++ frontend/lib/ui/post/widgets/post_screen.dart
@@ -49,137 +49,148 @@ class _PostFormState extends State<PostForm> {
 
   @override
   Widget build(BuildContext context) {
-    if (widget.viewModel.load.running) {
-      return const Center(child: CircularProgressIndicator());
-    } else if (widget.viewModel.load.error) {
-      final result = widget.viewModel.load.result! as Error;
-      return ErrorScreen(
-        message: formatExceptionMsg(result.error),
-        onRefresh: widget.viewModel.reload,
-      );
-    }
+    return SafeArea(
+      child: Scaffold(
+        body: ListenableBuilder(
+          listenable: widget.viewModel,
+          builder: (context, _) {
+            if (widget.viewModel.load.running) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (widget.viewModel.load.error) {
+              final result = widget.viewModel.load.result! as Error;
+              return ErrorScreen(
+                message: formatExceptionMsg(result.error),
+                onRefresh: widget.viewModel.reload,
+              );
+            }
 
-    return Padding(
-      padding: EdgeInsets.only(top: 24, bottom: 16),
-      child: ShadCard(
-        width: 400,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                ShadButton.ghost(
-                  height: 30,
-                  width: 120,
-                  onPressed: () {
-                    context.go('/');
-                  },
-                  child: const Text('Cancel'),
-                ),
-                ShadButton.ghost(
-                  height: 30,
-                  width: 120,
-                  enabled: _titleExists && _contentExists,
-                  onPressed: () async {
-                    if (formKey.currentState!.saveAndValidate()) {
-                      final result = await widget.viewModel.createPost.execute(
-                        Post(
-                          school: '',
-                          title: formKey.currentState!.value['title'],
-                          content: formKey.currentState!.value['content'],
-                          indexedAt: DateTime.now(),
-                        ),
-                      );
-
-                      switch (result) {
-                        case Ok<void>():
-                          logger.e('Successfully created post');
-                          if (context.mounted) {
+            return Padding(
+              padding: EdgeInsets.only(top: 24, bottom: 16),
+              child: ShadCard(
+                width: 400,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ShadButton.ghost(
+                          height: 30,
+                          width: 120,
+                          onPressed: () {
                             context.go('/');
-                          }
-                          return;
-                        case Error():
-                          logger.e('Error creating post in: $result');
-                          return;
-                      }
-                    }
-                  },
-                  child: const Text('Post'),
+                          },
+                          child: const Text('Cancel'),
+                        ),
+                        ShadButton.ghost(
+                          height: 30,
+                          width: 120,
+                          enabled: _titleExists && _contentExists,
+                          onPressed: () async {
+                            if (formKey.currentState!.saveAndValidate()) {
+                              final result =
+                                  await widget.viewModel.createPost.execute(
+                                Post(
+                                  school: '',
+                                  title: formKey.currentState!.value['title'],
+                                  content:
+                                      formKey.currentState!.value['content'],
+                                  indexedAt: DateTime.now(),
+                                ),
+                              );
+
+                              switch (result) {
+                                case Ok<void>():
+                                  logger.e('Successfully created post');
+                                  if (context.mounted) {
+                                    context.go('/');
+                                  }
+                                  return;
+                                case Error():
+                                  logger.e('Error creating post in: $result');
+                                  return;
+                              }
+                            }
+                          },
+                          child: const Text('Post'),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 30),
+                    Center(
+                      child: Column(
+                        children: [
+                          Text('Posting as'),
+                          Text(
+                            widget.viewModel.user?.school ?? 'School not found',
+                          ),
+                        ],
+                      ),
+                    ),
+                    ShadForm(
+                      key: formKey,
+                      child: Column(
+                        children: [
+                          ShadInputFormField(
+                            id: 'title',
+                            placeholder: Text(
+                              'Title',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyLarge!
+                                  .copyWith(
+                                    fontWeight: FontWeight.bold,
+                                    color: Theme.of(context).disabledColor,
+                                  ),
+                            ),
+                            decoration: ShadDecoration(
+                              border: ShadBorder.none,
+                              disableSecondaryBorder: true,
+                            ),
+                            validator: (String value) {
+                              if (value.isEmpty) {
+                                return "What's the title to your truth?";
+                              }
+                              return null;
+                            },
+                            onChanged: (String title) => setState(() {
+                              _titleExists = title.isNotEmpty;
+                            }),
+                          ),
+                          ShadInputFormField(
+                            id: 'content',
+                            placeholder: Text(
+                              'Speak your truth...',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyMedium!
+                                  .copyWith(
+                                    fontWeight: FontWeight.bold,
+                                    color: Theme.of(context).disabledColor,
+                                  ),
+                            ),
+                            decoration: ShadDecoration(
+                              border: ShadBorder.none,
+                              disableSecondaryBorder: true,
+                            ),
+                            validator: (String value) {
+                              if (value.isEmpty) {
+                                return "Don't you want to speak your truth?";
+                              }
+                              return null;
+                            },
+                            onChanged: (String content) => setState(() {
+                              _contentExists = content.isNotEmpty;
+                            }),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            SizedBox(height: 30),
-            Center(
-              child: Column(
-                children: [
-                  Text('Posting as'),
-                  ListenableBuilder(
-                    listenable: widget.viewModel,
-                    builder: (context, _) {
-                      final user = widget.viewModel.user;
-                      return Text(
-                        user?.school ?? 'School not found',
-                      );
-                    },
-                  ),
-                ],
               ),
-            ),
-            ShadForm(
-              key: formKey,
-              child: Column(
-                children: [
-                  ShadInputFormField(
-                    id: 'title',
-                    placeholder: Text(
-                      'Title',
-                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                            fontWeight: FontWeight.bold,
-                            color: Theme.of(context).disabledColor,
-                          ),
-                    ),
-                    decoration: ShadDecoration(
-                      border: ShadBorder.none,
-                      disableSecondaryBorder: true,
-                    ),
-                    validator: (String value) {
-                      if (value.isEmpty) {
-                        return "What's the title to your truth?";
-                      }
-                      return null;
-                    },
-                    onChanged: (String title) => setState(() {
-                      _titleExists = title.isNotEmpty;
-                    }),
-                  ),
-                  ShadInputFormField(
-                    id: 'content',
-                    placeholder: Text(
-                      'Speak your truth...',
-                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                            fontWeight: FontWeight.bold,
-                            color: Theme.of(context).disabledColor,
-                          ),
-                    ),
-                    decoration: ShadDecoration(
-                      border: ShadBorder.none,
-                      disableSecondaryBorder: true,
-                    ),
-                    validator: (String value) {
-                      if (value.isEmpty) {
-                        return "Don't you want to speak your truth?";
-                      }
-                      return null;
-                    },
-                    onChanged: (String content) => setState(() {
-                      _contentExists = content.isNotEmpty;
-                    }),
-                  ),
-                ],
-              ),
-            ),
-          ],
+            );
+          },
         ),
       ),
     );
