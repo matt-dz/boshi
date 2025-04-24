1696fc8694fa9478d087a4bebe7ddd105e485ce2
Alex Eum <aeum1003@gmail.com>
2025-04-23 20:51:39 -0400

    docs: added some.

--- tourbillon/src/methods/describe-generator.ts
+++ tourbillon/src/methods/describe-generator.ts
@@ -3,6 +3,10 @@ import { AppContext } from '../config'
 import { feedGenAlgos } from '../algos'
 import { AtUri } from '@atproto/syntax'
 
+/**
+ * https://docs.bsky.app/docs/api/app-bsky-feed-describe-feed-generator
+ * Adds an implementation of describe feed generator to our server.
+ */
 export default function (server: Server, ctx: AppContext) {
   server.app.bsky.feed.describeFeedGenerator(async () => {
     const feeds = Object.keys(feedGenAlgos).map((shortname) => ({
